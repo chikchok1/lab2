31dbd470d675110ee76a8086085ea9723a05c479
chikchok1 <tony09088@naver.com>
2025-11-24 11:56:02 +0900

    cleanup: 오래된 폴더 삭제

--- bash_practice/count_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# 현재 디렉토리 파일 개수 출력
-
-count=$(ls -1 | wc -l)
-echo "현재 디렉토리의 파일 개수: $count 개"
-
--- num7_makefile/lib/arith.h
+++
@@ -1,9 +0,0 @@
-#ifndef ARITH_H
-#define ARITH_H
-
-int add(int a, int b);
-int subtract(int a, int b);
-int multiply(int a, int b);
-double divide(int a, int b);
-
-#endif
